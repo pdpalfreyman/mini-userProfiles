0834db008900dedd4595891494949b8937b873a9
pdpalfreyman <14enigma@gmail.com>
2015-05-31 14:23:08 -0600

    updated

---
+++ controller.js
@@ -0,0 +1,11 @@
+var app = angular.module('userProfiles');
+
+app.controller('mainController', function($scope, mainService){
+
+	var getUsers = function(){
+		$scope.users = mainService.getUsers();
+	}
+
+	getUsers();
+
+});
